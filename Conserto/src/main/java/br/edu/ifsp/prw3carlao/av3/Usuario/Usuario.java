6637c34930ae24fb625aeb9282b54454cca44138
Raphael Martins <raphael.chiuso@gmail.com>
2024-12-03 11:22:20 -0300

    feat: criação das classes e tabela usuarios

---
+++ Conserto/src/main/java/br/edu/ifsp/prw3carlao/av3/Usuario/Usuario.java
@@ -0,0 +1,22 @@
+package br.edu.ifsp.prw3carlao.av3.Usuario;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+
+}
---
+++ Conserto/src/main/java/br/edu/ifsp/prw3carlao/av3/Usuario/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package br.edu.ifsp.prw3carlao.av3.Usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UsuarioRepository  extends JpaRepository <Usuario, Long> {
+    UserDetails findByLogin(String login);
+}
